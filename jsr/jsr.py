4694c24c825f963db79295184f68fc7f5bb4448a
Zachary Suarez <suarzac@gmail.com>
2023-01-22 23:26:07 -0800

    updates

--- jsr/jsr.py
+++ jsr/jsr.py
@@ -9,17 +9,14 @@ filename = f"{config.app_name}/{config.app_name}.py"
 import polars as pl
 
 #nba
-import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from .helpers import navbar
 
 nba_overview = "https://media.geeksforgeeks.org/wp-content/uploads/nba.csv"
-nba_data = pd.read_csv(nba_overview)
-college = sorted(nba_data["College"].unique().astype(str))
 #
-nba_data_pl = pl.read_csv(nba_overview)
-college = sorted(nba_data_pl["College"].unique().drop_nulls().cast(str))
+nba_data = pl.read_csv(nba_overview)
+college = sorted(nba_data["College"].unique().drop_nulls().cast(str))
 
 
 class State(pc.State):
@@ -48,30 +45,30 @@ class State(pc.State):
         #     & (nba_data["Salary"] < int(self.salary[1]))
         # ]
 
-        nba_pl = nba_data_pl.filter(
-            (nba_data_pl["Age"] > int(self.age[0]))
-            & (nba_data_pl["Age"] < int(self.age[1]))
-            & (nba_data_pl["Salary"] > int(self.salary[0]))
-            & (nba_data_pl["Salary"] < int(self.salary[1])) 
+        nba = nba_data.filter(
+            (nba_data["Age"] > int(self.age[0]))
+            & (nba_data["Age"] < int(self.age[1]))
+            & (nba_data["Salary"] > int(self.salary[0]))
+            & (nba_data["Salary"] < int(self.salary[1])) 
         )
 
         if self.college and self.college != "All":
             # nba = nba[nba["College"] == self.college]
-            nba_pl = nba_pl[nba_pl["College"] == self.college]
+            nba = nba[nba["College"] == self.college]
 
         if self.position and self.position != "All":
             # nba = nba[nba["Position"] == self.position]
-            nba_pl = nba_pl[nba_pl["Position"] == self.position]
+            nba = nba[nba["Position"] == self.position]
 
         # if nba.empty:
         #     return pd.DataFrame()
         # else:
         #     return nba.fillna("")
 
-        if nba_pl.is_empty():
+        if nba.is_empty():
             return pl.DataFrame()
         else:
-            return nba_pl.fill_nan("")
+            return nba.fill_nan("")
 
     @pc.var
     def scat_fig(self) -> go.Figure:
@@ -82,7 +79,7 @@ class State(pc.State):
             return go.Figure()
         else:
             return px.scatter(
-                nba,
+                nba.to_pandas(),
                 x="Age",
                 y="Salary",
                 title="NBA Age/Salary plot",
@@ -102,7 +99,7 @@ class State(pc.State):
             return go.Figure()
         else:
             return px.histogram(
-                nba, 
+                nba.to_pandas(), 
                 x="Age", 
                 y="Salary", 
                 title="Age/Salary Distribution"
@@ -152,7 +149,7 @@ def selection():
         ),
         width="100%",
     )
-#
+
 
 def index():
     return pc.center(
@@ -163,52 +160,51 @@ def index():
             pc.plotly(data=State.scat_fig, layout={"width": "1000", "height": "600"}),
             pc.plotly(data=State.hist_fig, layout={"width": "1000", "height": "600"}),
             pc.data_table(
-                data=nba_data,
+                data=nba_data.to_pandas(),
                 pagination=True,
                 search=True,
                 sort=True,
                 resizable=True,
-            ),
-            pc.divider(),
-            pc.responsive_grid(
-                pc.box(State.count, height="5em", width="5em", bg="lightgreen"),
-                pc.box(pc.text("Welcome to Pynecone!"), height="5em", width="5em", bg="lightblue"),
-                pc.box("Get started by editing ", pc.code(filename), height="5em", width="5em", bg="purple"),
-                pc.box(pc.link(
-                    "Check out our docs!",
-                    _hover={
-                        "color": "rgb(107,99,246)",
-                    },
-                ), height="5em", width="5em", bg="tomato"),
-                pc.box(height="5em", width="5em", bg="orange"),
-                pc.box(height="5em", width="5em", bg="yellow"),
-                columns=[3],
-                spacing="4",
-            ),
-            pc.divider(),
-            pc.hstack(
-                pc.button(
-                    "Decrement",
-                    color_scheme="red",
-                    border_radius="1em",
-                    on_click=State.decrement,
-                ),
-                pc.heading(State.count, font_size="2em"),
-                pc.button(
-                    "Increment",
-                    color_scheme="green",
-                    border_radius="1em",
-                    on_click=State.increment,
-                ),
-            ),
+            )
         ),
 
         padding_top="10%",
     )
 
-def api_test():
-    return {"Test": State}
-# Add state and page to the app.
+def topics():
+    return pc.vstack(
+        navbar(),
+        pc.hstack(
+            pc.button(
+                "Decrement",
+                color_scheme="red",
+                border_radius="1em",
+                on_click=State.decrement,
+            ),
+            pc.heading(State.count, font_size="2em"),
+            pc.button(
+                "Increment",
+                color_scheme="green",
+                border_radius="1em",
+                on_click=State.increment,
+            ),
+        ),
+    )
+
+
+def about():
+    return pc.text("About Page")
+
+
+def custom():
+    return pc.text("Custom Route")
+
+
 app = pc.App(state=State)
+
 app.add_page(index, title="Test JSR")
+app.add_page(topics)
+app.add_page(about)
+app.add_page(custom)
+
 app.compile()
