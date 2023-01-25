f5ffd6926e12250e6e160d688195888a9662cec3
Zachary Suarez <suarzac@gmail.com>
2023-01-24 19:45:26 -0800

    youtube videos dataset

--- jsr/jsr.py
+++ jsr/jsr.py
@@ -13,13 +13,16 @@ import plotly.express as px
 import plotly.graph_objects as go
 from .helpers import navbar
 
+youtube_overview = "data/USvideos.csv"
+youtube_data = pl.read_csv(youtube_overview)
+categories = sorted(youtube_data["category_id"].unique().drop_nulls().cast(str))
+
 nba_overview = "https://media.geeksforgeeks.org/wp-content/uploads/nba.csv"
 #
 nba_data = pl.read_csv(nba_overview)
 college = sorted(nba_data["College"].unique().drop_nulls().cast(str))
 
-
-class State(pc.State):
+class baseState(pc.State):
     """The app state."""
 
     count: int = 0
@@ -29,7 +32,71 @@ class State(pc.State):
 
     def decrement(self):
         self.count -= 1
-# nba
+
+class youtubeState(baseState):
+    # yt
+    channelTitle: str
+    categoryId: str
+    likes: list = [0, 25000000]
+    dislikes: list = [0, 25000000]
+
+    @pc.var
+    def df(self) -> pl.DataFrame:
+        """The data."""
+
+        yt = youtube_data.filter(
+            (youtube_data["likes"] > int(self.likes[0]))
+            & (youtube_data["likes"] < int(self.likes[1]))
+            & (youtube_data["dislikes"] > int(self.dislikes[0]))
+            & (youtube_data["dislikes"] < int(self.dislikes[1])) 
+        )
+
+        if self.channelTitle and self.channelTitle != "All":
+            yt = [yt["channel_title"] == self.channelTitle]
+
+        if self.categoryId and self.categoryId != "All":
+            yt = yt[yt["category_id"] == self.categoryId]
+
+        if yt.is_empty():
+            return pl.DataFrame()
+        else:
+            return yt.fill_nan("")
+
+    @pc.var
+    def scat_fig(self) -> go.Figure:
+        """The scatter figure."""
+        yt = self.df
+
+        if yt.is_empty():
+            return go.Figure()
+        else:
+            return px.bar(
+                yt.to_pandas(),
+                x="channel_title",
+                y="likes",
+                title="yt Age/Salary plot",
+                color=yt["category_id"],
+                hover_data=["channel_title"]
+            )
+
+    @pc.var
+    def hist_fig(self) -> go.Figure:
+        """The histogram figure."""
+        yt = self.df
+
+        if yt.is_empty():
+            return go.Figure()
+        else:
+            return px.histogram(
+                yt.to_pandas(), 
+                x="channel_title", 
+                y="likes", 
+                title="channel title and likes"
+            )
+
+class nbaState(baseState):
+    """The app state."""
+
     position: str
     college: str
     age: list = [0, 50]
@@ -38,12 +105,6 @@ class State(pc.State):
     @pc.var
     def df(self) -> pl.DataFrame:
         """The data."""
-        # nba = nba_data[
-        #     (nba_data["Age"] > int(self.age[0]))
-        #     & (nba_data["Age"] < int(self.age[1]))
-        #     & (nba_data["Salary"] > int(self.salary[0]))
-        #     & (nba_data["Salary"] < int(self.salary[1]))
-        # ]
 
         nba = nba_data.filter(
             (nba_data["Age"] > int(self.age[0]))
@@ -53,17 +114,10 @@ class State(pc.State):
         )
 
         if self.college and self.college != "All":
-            # nba = nba[nba["College"] == self.college]
             nba = nba[nba["College"] == self.college]
 
         if self.position and self.position != "All":
-            # nba = nba[nba["Position"] == self.position]
             nba = nba[nba["Position"] == self.position]
-
-        # if nba.empty:
-        #     return pd.DataFrame()
-        # else:
-        #     return nba.fillna("")
 
         if nba.is_empty():
             return pl.DataFrame()
@@ -105,41 +159,84 @@ class State(pc.State):
                 title="Age/Salary Distribution"
             )
 
+def youtubeSelection():
+    return pc.vstack(
+        pc.hstack(
+            pc.vstack(
+                pc.select(
+                    categories,
+                    placeholder="Select a category id. (All)",
+                    on_change=youtubeState.set_categoryId
+                ),
+                pc.select(
+                    college,
+                    placeholder="Select a channel title. (All)",
+                    on_change=youtubeState.set_channelTitle,
+                ),
+            ),
+            pc.vstack(
+                pc.vstack(
+                    pc.hstack(
+                        pc.badge("Min likes: ", youtubeState.likes[0]),
+                        pc.spacer(),
+                        pc.badge("Max likes: ", youtubeState.likes[1]),
+                    ),
+                    pc.range_slider(on_change_end=youtubeState.set_likes, min_=0, max_=25000000),
+                    align_items="left",
+                    width="100%",
+                ),
+                pc.vstack(
+                    pc.hstack(
+                        pc.badge("Min dislikes: ", youtubeState.dislikes[0], "M"),
+                        pc.spacer(),
+                        pc.badge("Max dislikes: ", youtubeState.dislikes[1], "M"),
+                    ),
+                    pc.range_slider(
+                        on_change_end=youtubeState.set_dislikes, min_=0, max_=25000000
+                    ),
+                    align_items="left",
+                    width="100%",
+                ),
+            ),
+            spacing="1em",
+        ),
+        width="100%",
+    )
 
-def selection():
+def nbaSelection():
     return pc.vstack(
         pc.hstack(
             pc.vstack(
                 pc.select(
                     ["C", "PF", "SF", "PG", "SG"],
                     placeholder="Select a position. (All)",
-                    on_change=State.set_position,
+                    on_change=nbaState.set_position,
                 ),
                 pc.select(
                     college,
                     placeholder="Select a college. (All)",
-                    on_change=State.set_college,
+                    on_change=nbaState.set_college,
                 ),
             ),
             pc.vstack(
                 pc.vstack(
                     pc.hstack(
-                        pc.badge("Min Age: ", State.age[0]),
+                        pc.badge("Min Age: ", nbaState.age[0]),
                         pc.spacer(),
-                        pc.badge("Max Age: ", State.age[1]),
+                        pc.badge("Max Age: ", nbaState.age[1]),
                     ),
-                    pc.range_slider(on_change_end=State.set_age, min_=18, max_=50),
+                    pc.range_slider(on_change_end=nbaState.set_age, min_=18, max_=50),
                     align_items="left",
                     width="100%",
                 ),
                 pc.vstack(
                     pc.hstack(
-                        pc.badge("Min Sal: ", State.salary[0] // 1000000, "M"),
+                        pc.badge("Min Sal: ", nbaState.salary[0] // 1000000, "M"),
                         pc.spacer(),
-                        pc.badge("Max Sal: ", State.salary[1] // 1000000, "M"),
+                        pc.badge("Max Sal: ", nbaState.salary[1] // 1000000, "M"),
                     ),
                     pc.range_slider(
-                        on_change_end=State.set_salary, min_=0, max_=25000000
+                        on_change_end=nbaState.set_salary, min_=0, max_=25000000
                     ),
                     align_items="left",
                     width="100%",
@@ -155,10 +252,10 @@ def index():
     return pc.center(
         pc.vstack(
             navbar(),
-            selection(),
+            nbaSelection(),
             pc.divider(),
-            pc.plotly(data=State.scat_fig, layout={"width": "1000", "height": "600"}),
-            pc.plotly(data=State.hist_fig, layout={"width": "1000", "height": "600"}),
+            pc.plotly(data=nbaState.scat_fig, layout={"width": "1000", "height": "600"}),
+            pc.plotly(data=nbaState.hist_fig, layout={"width": "1000", "height": "600"}),
             pc.data_table(
                 data=nba_data.to_pandas(),
                 pagination=True,
@@ -175,21 +272,20 @@ def topics():
     return pc.center(
         pc.vstack(
             navbar(),
-            selection(),
             pc.divider(),
             pc.hstack(
                 pc.button(
                     "Decrement",
                     color_scheme="red",
                     border_radius="1em",
-                    on_click=State.decrement,
+                    on_click=baseState.decrement,
                 ),
-                pc.heading(State.count, font_size="2em"),
+                pc.heading(baseState.count, font_size="2em"),
                 pc.button(
                     "Increment",
                     color_scheme="green",
                     border_radius="1em",
-                    on_click=State.increment,
+                    on_click=baseState.increment,
                 ),
             ),
         ),
@@ -198,18 +294,35 @@ def topics():
 
 
 def about():
-    return pc.text("About Page")
+    return pc.center(
+        pc.vstack(
+            navbar(),
+            youtubeSelection(),
+            pc.divider(),
+            pc.plotly(data=youtubeState.scat_fig, layout={"width": "1000", "height": "600"}),
+            pc.plotly(data=youtubeState.hist_fig, layout={"width": "1000", "height": "600"}),
+            pc.data_table(
+                data=youtube_data.to_pandas(),
+                pagination=True,
+                search=True,
+                sort=True,
+                resizable=True,
+            )
+        ),
+
+        padding_top="10%",
+    )
 
 
 def custom():
     return pc.text("Custom Route")
 
 
-app = pc.App(state=State)
+app = pc.App(state=baseState)
 
 app.add_page(index, title="Test JSR")
 app.add_page(topics)
-app.add_page(about)
+# app.add_page(about)
 app.add_page(custom)
 
 app.compile()
